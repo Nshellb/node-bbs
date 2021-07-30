42748ef0785c65fd7488838b85475a8b6051f3b8
Nshell <nshellb@gmail.com>
2021-07-30 21:41:13 +0900

    07302021 4.knext.js로 mysql과 연결하기 ./model/index.js 오류 수정

--- src/model/index.js
+++ src/model/index.js
@@ -3,7 +3,10 @@ const db = require('../database'); // 정의한 DB를 가져옴.
 const self = {};
 
 self.findUser = async () => {
+    const ret = await db.raw('SELECT * FROM USER');
     return ret[0]
 }
+
+const ret = db.raw('select now()').then((item) => {console.log(item[0])})
 
 module.exports = self;
